869eb58dc3c1beeb70ee4c21146791fb34cb553e
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-15 15:52:50 -0800

    Updates to full version release and clears out unused styling

--- events-block.php
+++ events-block.php
@@ -4,7 +4,7 @@
  * Description:       Imports upcoming events from Google Calendar and renders them on KCLS Voice
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            Two Dogs Web Development
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
